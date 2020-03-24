338b258b44711d556d7894968fc4e3f447624c74
Samir <samir@nevereatalone.io>
2020-03-24 18:47:25 +0100

    user integration post request

--- api/routes/integration.js
+++ api/routes/integration.js
@@ -1,12 +1,25 @@
-const express = require ('express');
+const express = require('express');
+const UserIntegration = require('../../model/UserIntegration');
+
 const router = express.Router();
 
 router.get('/', function (req, res) {
     res.send('ok');
 });
 
-router.post('/', function (req, res) {
-
+router.post('/', async function (req, res) {
+     try {
+         const {userId, name, data} = req.body;
+         await UserIntegration.findOneAndUpdate({userId, name}, {name, data, userId}, {upsert: true, runValidators: true});
+         res.send('ok');
+     }catch (e) {
+         console.error(e);
+         if(e.name === 'ValidationError'){
+             res.status(400).send('Bad request!');
+         }else{
+             res.status(500).send('Something went wrong!');
+         }
+     }
 });
 
 module.exports = router;
