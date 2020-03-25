d999fc103ceaf663c873a936343a18404cce2fbd
Samir <samir@nevereatalone.io>
2020-03-25 20:09:30 +0100

    update docker-compose network and close db connect for calendarSync job

--- jobs/google/calendarSync.js
+++ jobs/google/calendarSync.js
@@ -1,13 +1,14 @@
 const UserIntegration = require('../../model/UserIntegration');
-const {connectToDb} = require('../../utils/mongoose');
+const {connectToDb, disconnectFromDb} = require('../../utils/mongoose');
 const {performCalendarSync} = require('../../helpers/google');
 
 module.exports = async function (job) {
-    connectToDb();
-    console.log("Start user iterator job");
+    await connectToDb();
+    console.log("Start calendar sync job");
     const users = await UserIntegration.find({name:'google'});
     users.forEach(async u => {
         await performCalendarSync(u);
     });
+    disconnectFromDb();
     return "OK";
 };
