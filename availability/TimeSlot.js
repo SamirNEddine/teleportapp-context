adc21fdf79d4c7e572439fc1b9102c82d0950873
Samir <samir@nevereatalone.io>
2020-04-03 18:40:31 +0200

    fix a bug with TimeSlot duration

--- availability/TimeSlot.js
+++ availability/TimeSlot.js
@@ -1,18 +1,36 @@
 const util = require('util');
 
 module.exports = class TimeSlot {
-    start = 0;
-    end = 0;
-    duration = 0;
-    constructor(start, end, status){
-        this.start = start;
-        this.end = end;
-        this.status = status;
-        this.duration = end - start;
+    status = 'unassigned';
+    constructor(start, end, status) {
+        this._start = start;
+        this._end = end;
+        this._duration = end - start;
+        this.status = status ? status : 'unassigned';
+    }
+    set start(newStart) {
+        this._start = newStart;
+        this._duration = this._end - this._start;
+    }
+    get start() {
+        return this._start;
+    }
+    set end(newEnd) {
+        this._end = newEnd;
+        this._duration = this._end - this._start;
+    }
+    get end() {
+        return this._end;
+    }
+    get duration() {
+        return this._duration;
+    }
+    set duration(newDuration) {
+        //Do nothing
     }
     formatDuration() {
-        const numberOfHours = Math.floor(this.duration / (60*60*1000));
-        const numberOfMinutes = ((this.duration / (60*60*1000)) % 1)*60;
+        const numberOfHours = Math.floor(this._duration / (60*60*1000));
+        const numberOfMinutes = ((this._duration / (60*60*1000)) % 1)*60;
         return `${numberOfHours > 0 ? numberOfHours.toString() + ' hours' + (numberOfMinutes > 0 ? ' and ' : '') : ''}${numberOfMinutes > 0 ? numberOfMinutes.toString() + ' minutes' : ''}`;
     }
     [util.inspect.custom](depth, opts) {
