69e4f4f183d946c74c0cd29ff646e9654c1fea93
Samir <samir@nevereatalone.io>
2020-04-07 17:10:23 +0200

    create events on google calendar WIP

--- availability/calendarFromAvailability.js
+++ availability/calendarFromAvailability.js
@@ -1,6 +1,10 @@
+const UserIntegration = require('../model/UserIntegration');
+const {bookCalendarEventsFromTimeSlots} = require('../helpers/google');
 
 const updateCalendarWithTimeSlots = async function(userId, timeSlots) {
-
+    const googleCalendarIntegration = await UserIntegration.findOne({userId, name:'google'});
+    if(!googleCalendarIntegration) throw new Error('Bad request!');
+    await bookCalendarEventsFromTimeSlots(googleCalendarIntegration, timeSlots);
 };
 
 /** Exports **/
