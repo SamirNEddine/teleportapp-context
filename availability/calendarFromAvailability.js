35e03c024683c621e38cd9b289ea5188569b0e4c
Samir <samir@nevereatalone.io>
2020-04-20 13:37:25 +0200

    condition on when creating a calendar event

--- availability/calendarFromAvailability.js
+++ availability/calendarFromAvailability.js
@@ -2,13 +2,18 @@ const CalendarEvent = require('../model/CalendarEvent');
 const {scheduleTodayStatusChangeForUserIfNeeded} = require('../jobs/status');
 const {bookCalendarEvents} = require('../jobs/google');
 
+const shouldCreateEventForTimeSlot = function (timeSlot) {
+    return timeSlot.status === 'available' || timeSlot.status === 'focus';
+};
 const updateCalendarWithTimeSlots = async function(userId, timeSlots) {
     const calendarEvents = [];
     await Promise.all( timeSlots.map(async (timeSlot) => {
-        const calendarEvent = CalendarEvent.eventFromTimeSlot(timeSlot);
-        calendarEvent.userId = userId;
-        await calendarEvent.save();
-        calendarEvents.push(calendarEvent);
+        if(shouldCreateEventForTimeSlot(timeSlot)){
+            const calendarEvent = CalendarEvent.eventFromTimeSlot(timeSlot);
+            calendarEvent.userId = userId;
+            await calendarEvent.save();
+            calendarEvents.push(calendarEvent);
+        }
     }));
     await scheduleTodayStatusChangeForUserIfNeeded(userId);
     bookCalendarEvents(userId, calendarEvents);
