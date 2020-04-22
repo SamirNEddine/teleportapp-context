3a45f4c7344913602f3b8810d4f49edce1d0c78e
Samir <samir@nevereatalone.io>
2020-04-22 15:24:01 +0200

    deployment adjustment

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const dbConnectURL = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_BASE_URL}:${process.env.DB_PORT}/${process.env.MAIN_DB_NAME}`;
+const dbConnectURL = process.env.DB_FULL_URL;
 module.exports.connectToDb = function () {
     return new Promise(async function (resolve, reject) {
         try{
             await mongoose.connect(dbConnectURL, {useNewUrlParser: true, useFindAndModify: false, useCreateIndex:true, useUnifiedTopology:true});
-            console.info('Successfully connected to the database: ' + process.env.MAIN_DB_NAME + ' using username: '+process.env.DB_USERNAME + ' on port: '+process.env.DB_PORT);
+            console.info('Successfully connected to the database');
             resolve();
         }catch(error){
             console.error(error);
