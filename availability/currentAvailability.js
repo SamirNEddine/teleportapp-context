1cb5a83550baecbb9df69562cb7d0909ac39522a
Samir <samir@nevereatalone.io>
2020-05-13 18:38:25 +0200

    update current availability WIP

--- availability/currentAvailability.js
+++ availability/currentAvailability.js
@@ -1,4 +1,5 @@
-const {getAvailabilityForToday} = require('./todayAvailability');
+const CalendarEvent = require('../model/CalendarEvent');
+const {getAvailabilityForToday, invalidatedCachedTodayAvailability} = require('./todayAvailability');
 const {performChangeStatusForUser} = require('../jobs/status');
 
 const getCurrentAvailability = async function (userContextParams) {
@@ -14,14 +15,52 @@ const setCurrentAvailability = async function (userContextParams, newAvailabilit
     const availability = await getAvailabilityForToday(userContextParams.userId);
     const current = availability.current();
     if(current.status !== newAvailability){
+        //Update Teleport Events
+        const updates = [];
+        const now = new Date();
+        const eventsToUpdate = await CalendarEvent.findEventsBetweenTwoTimestamps(userContextParams.userId, current.start, current.end);
+        eventsToUpdate.forEach(event => {
+            if(event.startDateTime.getTime() >= now.getTime()){
+                //It means it's not started yet but part of the previous availability slot computation. Remove it.
+                updates.push({
+                    deleteOne: {
+                        filter: {_id: event.id}
+                    }
+                });
+            }else if(event.endDateTime.getTime() > now.getTime()){
+                //It means it was ongoing
+                updates.push({
+                    updateOne: {
+                        filter: {_id: event.id},
+                        update: {
+                            endDateTime: now
+                        }
+                    }
+                });
+            }
+
+        });
+        if(updates.length > 0) {
+            await CalendarEvent.bulkWrite(updates);
+        }
+
+        //Create a new Event for the new current availability
+        current.start = now.getTime();
+        current.status = newAvailability;
+        const calendarEvent = CalendarEvent.eventFromTimeSlot(current);
+        calendarEvent.userId = userContextParams.userId;
+        await calendarEvent.save();
+
+        //Return the new availability
         const next = availability.next();
-        //Merge with next status if applicable
+        //Merge with next status if needed
         if(next && next.status === current.status){
             current.end = next.end;
         }
-        current.status = newAvailability;
+
         //Update Slack status
         performChangeStatusForUser(userContextParams.userId, current);
+        await invalidatedCachedTodayAvailability(userContextParams.userId);
     }
     return current.toObject();
 };
@@ -37,3 +76,4 @@ const getNextAvailability = async function (userContextParams) {
 
 module.exports.getCurrentAvailability = getCurrentAvailability;
 module.exports.setCurrentAvailability = setCurrentAvailability;
+module.exports.getNextAvailability = getNextAvailability;
